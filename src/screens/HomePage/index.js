36685e69d062c6a1408d19eafeaebe8f8817e542
Luan <luanfrancavieira@gmail.com>
2020-03-10 21:25:56 -0300

    jogos salvos na asyncStorage e buscar por jogo

--- src/screens/HomePage/index.js
+++ src/screens/HomePage/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Container, Search, Input, InitSearch} from './styled'
-import {Text, View, FlatList} from 'react-native'
+import {FlatList, Keyboard, View, Text} from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'  
 import Icon from 'react-native-vector-icons/Octicons'
 import Game from './../../components/Game'
 
@@ -8,29 +9,62 @@ Icon.loadFont()
 
 export default () =>
 {
-    const games = [
-        {name: 'The last of us', platform: ['Sony'], category: ['adventure', 'action'], status: 3, favority: true},
-        {name: 'The Witcher 3', platform: ['Sony', 'Nintendo', 'Xbox', 'PC'], category: ['adventure', 'action'], status: 3, favority: true},
-        {name: 'Crash Bandicoot Remastered', platform: ['Sony', 'Nintendo', 'Xbox', 'PC'], category: ['adventure'], status: 2, favority: false},
-        {name: 'Resident Evil 2 Remake', platform: ['Sony', 'Xbox', 'PC'], category: ['adventure', 'action', 'horror'], status: 2, favority: false},
-        {name: 'The last of us 2', platform: ['Sony'], category: ['adventure', 'action'], status: 1, favority: true},
-        {name: 'UFC 3', platform: ['Sony', 'Xbox'], category: ['fight'], status: 3, favority: false},
-    ]
+    const [games, setGames] = useState([])
+    const [search, setSearch] = useState('')
+    const [viewGames, setViewGames] = useState([])
+
+    const searchGames = async() => 
+    {
+        setSearch('')
+        Keyboard.dismiss()
+
+        const response = games.filter(e => {
+            if(e.name.toLowerCase().indexOf(search.toLowerCase()) !== -1) 
+                return e  
+        })
+        setViewGames(response)
+    }
+
+    useEffect(() => {
+        const handleGames = async () =>
+        {
+            /*await AsyncStorage.setItem('@games', JSON.stringify([
+                {name: 'The last of us', platform: ['Playstation'], category: ['action', 'adventure'], status: 2, favority: true}, 
+                {name: 'The Witcher 3', platform: ['Playstation', 'Xbox', 'PC', 'Nintendo'], category: ['action', 'adventure'], status: 2, favority: true}, 
+                {name: 'Resident Evil 2 Remake', platform: ['Playstation', 'Xbox', 'PC'], category: ['action', 'adventure', 'horror'], status: 2, favority: false}, 
+                {name: 'The banner saga 3', platform: ['Playstation', 'Xbox', 'PC', 'Nintendo'], category: ['strategy', 'adventure'], status: 1, favority: false}, 
+                {name: 'The last of us 2', platform: ['Playstation'], category: ['action', 'adventure'], status: 3, favority: true},
+            ]))*/
+            const gamesStorage = JSON.parse(await AsyncStorage.getItem('@games'))
+            setGames(gamesStorage.length === 0 ? [] : gamesStorage)
+            setViewGames(gamesStorage.length === 0 ? [] : gamesStorage)
+        }
+
+        handleGames()
+    }, [])
 
     return (
         <Container>
             <Search>
-                <Input placeholder="Buscar..." />
-                <InitSearch>
+                <Input placeholder="Buscar..." onChangeText={e => setSearch(e)} />
+                <InitSearch onPress={searchGames}>
                     <Icon name="search" size={25} color="#fff" />
                 </InitSearch>
             </Search>
 
-            <FlatList
-                data={games}
-                keyExtractor={games => games.name}
-                renderItem={games => <Game data={games} />}
-            />
+            {
+                viewGames.length > 0 
+                ?
+                <FlatList
+                    data={viewGames}
+                    keyExtractor={viewGames => viewGames.name}
+                    renderItem={viewGames => <Game data={viewGames} />}
+                />
+                :
+                <View>
+                    <Text>Nenhum jogo encontrado!</Text>
+                </View>
+            }
         </Container>
     )
 }
